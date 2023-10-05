3042bfd7ac4b9f1adbce44dbf78f40fab665e154
MamonYoshioka <141148990+MamonYoshioka@users.noreply.github.com>
2023-10-05 08:03:59 +0000

    [add]認証機能の更新

--- app/controllers/end_user/registrations_controller.rb
+++ app/controllers/end_user/registrations_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class EndUser::RegistrationsController < Devise::RegistrationsController
-  before_action :configure_permitted_parameters, if: devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
