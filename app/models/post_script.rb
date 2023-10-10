a7c32776cd53a32c16e4d48ff0089a698a7b5356
MamonYoshioka <141148990+MamonYoshioka@users.noreply.github.com>
2023-10-10 03:21:21 +0000

    [add&update]いいね機能の実装とレイアウト更新

--- app/models/post_script.rb
+++ app/models/post_script.rb
@@ -1,2 +1,8 @@
 class PostScript < ApplicationRecord
+  belongs_to :end_user
+  has_many :favorites, dependent: :destroy
+
+  def favorited_by?(end_user)
+    favorites.exists?(end_user_id: end_user.id)
+  end
 end
