3416f2f6aedd7a61f9ad1dd1d8a948c810aa0c58
MamonYoshioka <141148990+MamonYoshioka@users.noreply.github.com>
2023-10-13 10:37:03 +0000

    [add]ゲストログイン機能

--- app/controllers/end_user/sessions_controller.rb
+++ app/controllers/end_user/sessions_controller.rb
@@ -3,6 +3,12 @@
 class EndUser::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
+  def guest_sign_in
+    end_user = EndUser.guest
+    sign_in end_user
+    redirect_to root_path, notice: 'ゲストユーザーとしてログインしました。'
+  end
+
   # GET /resource/sign_in
   # def new
   #   super
--- app/models/end_user.rb
+++ app/models/end_user.rb
@@ -12,4 +12,13 @@ class EndUser < ApplicationRecord
 
   # 追記投稿についてのコメント機能とのアソシエーション
   has_many :post_script_comments, dependent: :destroy
+
+  # 以下を追加
+  def self.guest
+    find_or_create_by!(email: 'guest@example.com') do |end_user|
+      end_user.password = SecureRandom.urlsafe_base64
+      # user.confirmed_at = Time.now  # Confirmable を使用している場合は必要
+      # 例えば name を入力必須としているならば， user.name = "ゲスト" なども必要
+    end
+  end
 end
