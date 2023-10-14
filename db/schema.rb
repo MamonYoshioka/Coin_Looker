121c4620c724eb3766028b2da6222353d2ddddd1
MamonYoshioka <141148990+MamonYoshioka@users.noreply.github.com>
2023-10-14 13:54:53 +0000

    [add]googlemap機能追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_10_11_135724) do
+ActiveRecord::Schema.define(version: 2023_10_14_132300) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -51,6 +51,9 @@ ActiveRecord::Schema.define(version: 2023_10_11_135724) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "loker_name"
+    t.string "address"
+    t.float "latitude"
+    t.float "longitude"
   end
 
   create_table "favorites", force: :cascade do |t|
